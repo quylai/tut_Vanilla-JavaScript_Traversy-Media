9408a17761fa64c72db569b0ca34c1079dbe5fec
quylai <quylai@gmail.com>
2020-03-06 19:01:01 -0600

    saved

--- 09/initial/main.js
+++ 09/initial/main.js
@@ -121,10 +121,10 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // Combine Methods
 
-const combined = ages
-  .map(age => age * 2)
-  .filter(age => age >= 40)
-  .sort((a, b) => a - b)
-  .reduce((a, b) => a + b, 0);
+// const combined = ages
+//   .map(age => age * 2)
+//   .filter(age => age >= 40)
+//   .sort((a, b) => a - b)
+//   .reduce((a, b) => a + b, 0);
 
-console.log(combined);
+// console.log(combined);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tut_Vanilla-JavaScript_Traversy-Media
+
+Tutorial src:
+  https://www.youtube.com/playlist?list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX
